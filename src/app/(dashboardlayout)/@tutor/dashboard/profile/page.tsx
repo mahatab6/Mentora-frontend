34595efa7df441c0ad610b8ddd8b27c86cbd54dd
mahatav6 <mahatabcall56@gmail.com>
2026-01-29 10:40:08 +0600

    manage profile page create

--- src/app/(dashboardlayout)/@tutor/dashboard/profile/page.tsx
+++ src/app/(dashboardlayout)/@tutor/dashboard/profile/page.tsx
@@ -1,6 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function profilePage() {
   return (
-    <div>Tutor profile Page</div>
-  )
+    <div>
+      <div className="flex justify-between w-full items-center gap-1 px-4 py-4 lg:gap-2 lg:px-6">
+        <h2>My Profile</h2>
+        <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
+          <Link href={"/dashboard/manage-profile"}>Manage Profile</Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
